0287fa755cbed4b3f8265b028cf7cb019f7a599f
Jackson Asaro <jacksonasaro@Jacksons-MacBook-Pro.local>
2020-11-23 17:40:13 +0000

    begin adding new ejs files in attempt to restart and streamline project

--- app.js
+++ app.js
@@ -10,158 +10,27 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 const deckOfCards = require("deck-o-cards");
 
-var playerHand = [];
-var playerHand2 = [];
-var dealerHand = [];
-var options = [];
-var bank = 100;
-var bet = 0;
+bank = 100;
+username = "jl4414";
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port 3000");
 });
 
-app.get("/", function (req, res) {
-  if (playerHand.length == 0) {
-    res.render("bet", {
-      bank: bank,
-      message: "",
-    });
-  } else {
-    if (bet * 2 <= bank && playerHand[0].number == playerHand[1].number) {
-      options = ["hit", "stand", "double", "split"];
-    } else if (bet * 2 <= bank) {
-      options = ["hit", "stand", "double"];
-    } else {
-      options = ["hit", "stand"];
-    }
-    res.render("index", {
-      bet: bet,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      options: options,
-      playerBestTotal: bestTotal(playerHand),
-    });
-  }
-});
-
-app.get("/double", function (req, res) {
-  bet = bet * 2;
-  playerHand = playerHand.concat(Array.from(deckOfCards.randomizedDeck()).slice(0, 1));
-  if (sumHand(playerHand) > 21) {
-    bank -= bet;
-    res.render("final", {
-      title: "You Busted",
-      bet: "",
-      bank: bank,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      playerBestTotal: bestTotal(playerHand),
-      dealerBestTotal: bestTotal(dealerHand),
-    });
-  } else {
-    res.redirect("/stand");
-  }
-});
-
-app.get("/split", function(req, res){
-  playerHand2 = [playerHand[1]];
-  playerHand.pop();
-  res.render("splitoneindex", {
-    bet: bet,
-    dealerHand: dealerHand,
-    playerHand: playerHand,
-    options: ["Hit", "Stand"],
-    playerBestTotal: bestTotal(playerHand),
-  })
+app.get("/", function(req, res){
+  res.render("deal", {
+    bank: bank,
+    username: username,
+    bet: 5,
+    playerHand: [{ number: 2, name: 'two', type: '♠️' }, { number: 9, name: 'nine', type: '♦️' },],
+    dealerHand: [{ number: 2, name: 'two', type: '♠️' }],
+    bestTotal: bestTotal([{ number: 2, name: 'two', type: '♠️' }, { number: 9, name: 'nine', type: '♦️' },]),
+    options: ["Hit", "Stand", "Double", "Split"]
+  });
 })
 
-app.post("/bet", function (req, res) {
-  bet = Number(req.body.bet);
-  if (bet > bank || bet < 0) {
-    res.render("bet", {
-      bank: bank,
-      message: "Bet must be between 0 and " + bank,
-    });
-  } else {
-    // playerHand = Array.from(deckOfCards.randomizedDeck()).slice(0, 2);
-    playerHand = [{ number: 2, name: 'two', type: '♠️' }, { number: 2, name: 'two', type: '❤️' }];
-    dealerHand = Array.from(deckOfCards.randomizedDeck()).slice(0, 2);
-    res.redirect("/");
-  }
 
 
-});
-
-app.get("/hit", function (req, res) {
-  playerHand = playerHand.concat(Array.from(deckOfCards.randomizedDeck()).slice(0, 1));
-  if (sumHand(playerHand) > 21) {
-    bank -= bet;
-    res.render("final", {
-      title: "You Busted",
-      bank: bank,
-      bet: "",
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      playerBestTotal: bestTotal(playerHand),
-      dealerBestTotal: bestTotal(dealerHand),
-    });
-  } else {
-    res.render("index", {
-      bet: bet,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      options: ["hit", "stand"],
-      playerBestTotal: bestTotal(playerHand),
-    });
-  }
-});
-
-app.get("/stand", function (req, res) {
-  while (bestTotal(dealerHand) < 17) {
-    dealerHand = dealerHand.concat(Array.from(deckOfCards.randomizedDeck()).slice(0, 1));
-  }
-  if (bestTotal(dealerHand) == bestTotal(playerHand)) {
-    res.render("final", {
-      title: "You pushed",
-      bet: "",
-      bank: bank,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      playerBestTotal: bestTotal(playerHand),
-      dealerBestTotal: bestTotal(dealerHand),
-    });
-  } else if ((bestTotal(dealerHand) > bestTotal(playerHand)) && bestTotal(dealerHand) < 22) {
-    bank -= bet;
-    res.render("final", {
-      title: "You lost",
-      bet: "$" + bet,
-      bank: bank,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      playerBestTotal: bestTotal(playerHand),
-      dealerBestTotal: bestTotal(dealerHand),
-    });
-  } else {
-    bank += bet;
-    res.render("final", {
-      title: "You won",
-      bet: "$" + bet,
-      bank: bank,
-      playerHand: playerHand,
-      dealerHand: dealerHand,
-      playerBestTotal: bestTotal(playerHand),
-      dealerBestTotal: bestTotal(dealerHand),
-    });
-  }
-});
-
-app.get("/again", function (req, res) {
-  bet = 0;
-  playerHand = [];
-  dealerHand = [];
-  res.redirect("/");
-});
 
 function sumHand(hand) {
   sum = 0;

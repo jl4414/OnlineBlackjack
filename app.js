b762d82c040fae1d0a6864a94b402f24ea995ede
jl4414 <jacksonasaro@gmail.com>
2021-02-25 13:26:35 +0000

    change file structure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const db = require("db");
+const db = require(__dirname + "/db");
 
 app.use(bodyParser.urlencoded({
   extended: true
